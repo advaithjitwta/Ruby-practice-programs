4c0d5cebf4205bc892976c26f3e2deaa4eeca3ec
Advaith V <82958059+advaithjitwta@users.noreply.github.com>
2022-04-07 09:15:26 +0530

    Add files via upload

---
+++ Programs-master/Check if a String is a Palindrome.rb
@@ -0,0 +1,10 @@
+variable = "abaersses"
+def check_palindromic(variable)
+    if variable.reverse == variable 
+      puts "#{ variable } is a palindrome."
+    else 
+      puts "#{ variable } is not a palindrome."
+    end
+  end
+
+  
---
+++ Programs-master/tempCodeRunnerFile.rb
@@ -0,0 +1,2 @@
+rows, cols = 5,3
+p grid = Array.new(rows) { Array.new(cols) }
